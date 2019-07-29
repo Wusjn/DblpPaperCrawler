c3fd94bd005711ada087450b2989c939125d4c7d
1500012880 <1500012880@pku.edu.cn>
2019-07-29 13:42:26 +0800

    fix

--- getPapers.py
+++ getPapers.py
@@ -7,6 +7,7 @@ if len(sys.argv) <= 1:
 
 seedUrl = sys.argv[1]
 
+os.makedirs('crawledData')
 os.system('rm crawledData/pubUrls.json')
 os.system('scrapy crawl example -o crawledData/pubUrls.json -a seedUrl=' + seedUrl)
 os.system('python downloadScripts/downloadPdf.py')
